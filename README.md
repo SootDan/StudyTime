2677ea4e704182813eedd07cb8420bacda1e109b
Daniel B. <124447854+SootDan@users.noreply.github.com>
2024-09-21 17:20:13 +0200

    Loading Subjects with Statistics + Refactoring

--- README.md
+++ README.md
@@ -6,23 +6,27 @@ TBD
 WIP  
   
 ### Current Features:
-- SQLite Database 
+- SQLite Database  
+- Terminal Statistics Output
   
 ### To-Do:
 WIP  
   
 ### Current Bugs:
-WIP  
+- User input not clean, causes crashing
   
 ## About
 ### Documentation
 My main goal for this app was creating easily readable and clean code. I focused on making sure the variables were type safe and the methods/classes had proper doc strings that explained their use succinctly. I felt particularly inspired by the strong type safety of C#, which I have been using a lot for personal projects recently.  
-I started out making an SQL database that sorted each subject into its own respective table. I noticed that the output strings were cluttering up my code, so I offloaded them to a json file.
+I started out making an SQL database that sorted each subject into its own respective table. I noticed that the output strings were cluttering up my code, so I offloaded them to a json file. Then I formatted the SQL database into a readable string using pandas.  
+Cleaned the code and tried to optimise it into smaller, more readable bits. Made SQL more modular; it now supports multiple databases at the same time if needed.  
+Enums served to declutter the SQL within the code.
   
 ### Creator
 This project is created and maintained solely by myself.  
   
 ## Sources
-[SQLite Python Documentation](https://docs.python.org/3/library/sqlite3.html)
-[SQLite Time Documentation](https://www.sqlite.org/lang_datefunc.html)
-[Creating Arch Packages](https://wiki.archlinux.org/title/Creating_packages)
+[SQLite Python Documentation](https://docs.python.org/3/library/sqlite3.html)  
+[SQLite Time Documentation](https://www.sqlite.org/lang_datefunc.html)  
+[Pandas Documentation](https://pandas.pydata.org/docs/reference/api/pandas.read_sql_query.html#pandas.read_sql_query)  
+[Creating Arch Packages](https://wiki.archlinux.org/title/Creating_packages)  
--- app.py
+++ app.py
@@ -1,74 +1,116 @@
 """
-SQlite3: Database
-Datetime: Converts current time to ISO 8601 for SQL
-Json: Imports strings to be used in the program; keeps code clean.
+Handles the table and SQL queries.
 """
-from sqlite3 import Connection, Cursor, connect, Row
-from datetime import date
-from json import load
+from sqlite3 import Connection, Cursor, connect, PARSE_DECLTYPES    # Database
+from datetime import date   # Converts current time to ISO 8601 for SQL.
+from json import load   # Imports strings to be used in the program; keeps code clean.
+from enum import Enum   # Eases access to SQL and further decluttering.
+#from pandas import read_sql_query  # Formats SQL tables into readable strings.
 
 class StudyTime:
     """
     The app.
     """
-    def __init__(self) -> None:
-        self.connection: Connection = connect("studytime.sqlite")
-        self.connection.row_factory = Row
+    def __init__(self, db: str) -> None:
+        self.connection: Connection = connect(f"{db}.sqlite", detect_types=PARSE_DECLTYPES)
         self.cursor: Cursor = self.connection.cursor()
-        self.subjects: dict[str, float] = {}
+        self.sql_commands: Enum = Enum("SQLEnum", ["SUBJECTS"])
+        self.subjects: list[Cursor] = self.sql_parser("SUBJECTS")
 
 
     def initialize_app(self) -> None:
         """
         Initializes/creates the SQL databank and adds all subjects into a list.
         """
-        subject_count: list[Cursor] = \
-            self.cursor.execute("SELECT name FROM sqlite_master").fetchall()
-
-        if len(subject_count) == 0:
+        if len(self.subjects) == 0:
             self.create_subjects()
+            self.initialize_app()
         else:
-            self.load_subjects(subject_count)
+            self.load_subjects()
+
+        self.app_navigator()
+
+
+    def app_navigator(self) -> None:
+        """
+        Navigates the app and lets the user edit and manipulate data.
+        """
+        #TODO: Clean user input and implement method
+        self.json_strings("welcome_navigation")
+        db_id: int = int(input("ID: "))
+        subj_current: list[Cursor] = self.sql_parser("SUBJECTS")[db_id][0]
+        print(subj_current)
 
 
     def create_subjects(self) -> None:
         """
         Creates the study subjects and loads them into code.
         """
-        print(self.output_strings("create_subjects"))
-        subject_name: str = ""
-        subject_time: float = 0.0
+        self.json_strings("create_subjects")
+        subj_name: str = ""
+        subj_time: float = 0.0
+        subjects: dict[str, float] = {}
 
         while True:
-            print(self.output_strings("exit_create_subjects"))
-            subject_name: str = input("Name: ")
-            if subject_name.upper() == "D":
+            self.json_strings("exit_create_subjects")
+            subj_name: str = input("Name: ")
+            if subj_name.upper() == "D":    # Done
                 break
-            subject_time: float = input("Time (in hours): ") * 60
-            self.subjects.update({subject_name: subject_time})
+            if subj_name.upper() == "M":    # Mistake
+                rm_key: dict = list(subjects)[-1]
+                subjects.pop(rm_key)
+            else:
+                #TODO: Clean user input
+                subj_time: float = float(input("Time (in hours): "))
+                subjects.update({subj_name: subj_time})
 
-        for subject_name, time_req in self.subjects.items():
-            self.cursor.execute(f"CREATE TABLE {subject_name} (date DATE, time INTEGER)")
-            self.cursor.execute(f"INSERT INTO {subject_name} VALUES (?, ?)", \
-                (date.today(), time_req))
+        for subj_name, time_req in subjects.items():
+            self.cursor.execute(f"""CREATE TABLE {subj_name}
+            (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date DATE, time FLOAT)""")
+            self.cursor.execute(f"INSERT INTO {subj_name} VALUES (?, ?, ?)", \
+                (None, date.today(), time_req))
         self.connection.commit()
 
 
-    def load_subjects(self, subjects: list[Cursor]) -> None:
+    def load_subjects(self) -> None:
         """
         Loads the subjects and stats into the code.
         """
-        for subject in subjects:
-            print(self.cursor.execute(f"SELECT * FROM {subject[0]}").fetchall()[0])
-            #TODO: Fix method
+        i: int = 0
+        for subject in self.subjects:
+            subj_name: str = f"{subject[0]} (ID: {i})"
+            subj_time_req: float = self.cursor.execute(f"SELECT time FROM {subject[0] \
+                } WHERE id=1").fetchone()[0]
+            subj_time_spent: float = self.cursor.execute(f"SELECT SUM(time) FROM {subject[0] \
+                } WHERE NOT id=1").fetchone()[0]
+            subj_time_spent = subj_time_spent if subj_time_spent is not None else 0.0
+
+            print(f"SUBJECT:\t\t{subj_name}")
+            print(f"HOURS REQUIRED:\t\t{subj_time_req} HOURS")
+            print(f"HOURS SPENT:\t\t{subj_time_spent} HOURS")
+            print(f"HOURS LEFT:\t\t{subj_time_req - subj_time_spent} HOURS")
+            print("############\t\t############")
+            i += 1
 
 
-    def output_strings(self, message: str) -> str:
+    def json_strings(self, message: str) -> None:
         """
         Transfers the json strings to the code to stop clutter.
         """
         with open("strings.json", "r", encoding="utf-8") as json:
-            return load(json).get(message)
+            print(load(json).get(message))
 
-study_time = StudyTime()
+
+    def sql_parser(self, command: Enum) -> list[Cursor]:
+        """
+        SQL command handler.
+        """
+        if command == "SUBJECTS":
+            return self.cursor.execute("""SELECT name FROM sqlite_master
+            WHERE NOT name=\"sqlite_sequence\"""").fetchall()
+        else:
+            return []
+
+
+study_time = StudyTime("studytime")
 study_time.initialize_app()
